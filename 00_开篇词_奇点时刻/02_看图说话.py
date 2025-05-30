ebd97d23c4d919aa75074e1e0cad9593ef6fce4d
ShuoSun <shuo.sun@veeva.com>
2025-05-30 13:44:58 +0800

    Refactor image captioning script to use ChatOllama model and update initialization

--- 00_开篇词_奇点时刻/02_看图说话.py
+++ 00_开篇词_奇点时刻/02_看图说话.py
@@ -1,23 +1,15 @@
-'''欢迎来到LangChain实战课
-https://time.geekbang.org/column/intro/100617601
-作者 黄佳'''
+"""
+本项目使用开源多模态模型 llava:13b，通过本地 Ollama 部署，支持图像输入与文本输出任务。
+"""
 
-from dotenv import load_dotenv  # 用于加载环境变量
-load_dotenv()  # 加载 .env 文件中的环境变量
-
-#---- Part 0 导入所需要的类
-import os
 import requests
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from langchain.tools import BaseTool
-# from langchain import OpenAI
-from langchain_openai import OpenAI
 from langchain.agents import initialize_agent, AgentType
-# from langchain.agents import create_react_agent # 未来需要改成下面的设计
+from langchain.tools import BaseTool
+from langchain_ollama import ChatOllama
+from transformers import BlipProcessor, BlipForConditionalGeneration
 
-#---- Part I 初始化图像字幕生成模型
-# 指定要使用的工具模型（HuggingFace中的image-caption模型）
+# 指定要使用的工具模型（HuggingFace的image-caption模型）
 hf_model = "Salesforce/blip-image-captioning-large"
 
 # 初始化处理器和工具模型
@@ -26,13 +18,12 @@ processor = BlipProcessor.from_pretrained(hf_model)
 # 然后我们初始化工具模型本身
 model = BlipForConditionalGeneration.from_pretrained(hf_model)
 
-#---- Part II 定义图像字幕生成工具类
-class ImageCapTool(BaseTool):
-   
-    name = "Image captioner"
-    description = "为图片创作说明文案."
 
-    def _run(self, url: str):
+class ImageCapTool(BaseTool):
+    name: str = "Image captioner"
+    description: str = "为图片创作说明文案."
+
+    def _run(self, url: str) -> str:
         # 下载图像并将其转换为PIL对象
         image = Image.open(requests.get(url, stream=True).raw).convert('RGB')
         # 预处理图像
@@ -42,14 +33,16 @@ class ImageCapTool(BaseTool):
         # 获取字幕
         caption = processor.decode(out[0], skip_special_tokens=True)
         return caption
-    
+
     def _arun(self, query: str):
         raise NotImplementedError("This tool does not support async")
 
-#---- PartIII 初始化并运行LangChain智能体
-# 设置OpenAI的API密钥并初始化大语言模型（OpenAI的Text模型）
-# os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
-llm = OpenAI(temperature=0.2)
+
+llm = ChatOllama(
+    base_url="http://localhost:11434",  # 服务地址
+    model="qwen3:8b",  # 模型名称
+    temperature=0
+)
 
 # 使用工具初始化智能体并运行
 tools = [ImageCapTool()]
@@ -59,11 +52,5 @@ agent = initialize_agent(
     llm=llm,
     verbose=True,
 )
-# 未来需要改成下面的设计
-# agent = create_react_agent(
-#     tools=tools,
-#     llm=llm,
-# )
 img_url = 'https://mir-s3-cdn-cf.behance.net/project_modules/hd/eec79e20058499.563190744f903.jpg'
-# agent.run(input=f"{img_url}\n请创作合适的中文推广文案")
 agent.invoke(input=f"{img_url}\n请创作合适的中文推广文案")
