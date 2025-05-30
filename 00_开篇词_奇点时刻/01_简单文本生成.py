662bbbba905b0eb1fe0f1fc40a85d15f81f8e2f5
ShuoSun <shuo.sun@veeva.com>
2025-05-30 13:44:49 +0800

    Refactor text generation script to use ChatOllama model and update API calls

--- 00_开篇词_奇点时刻/01_简单文本生成.py
+++ 00_开篇词_奇点时刻/01_简单文本生成.py
@@ -1,16 +1,13 @@
-'''欢迎来到LangChain实战课
-https://time.geekbang.org/column/intro/100617601
-作者 黄佳'''
+"""
+使用Ollama的ChatOllama模型生成文本
+"""
 
-from dotenv import load_dotenv  # 用于加载环境变量
-load_dotenv()  # 加载 .env 文件中的环境变量
+from langchain_ollama import ChatOllama
 
-# import os
-# os.environ["OPENAI_API_KEY"] = '你的Open AI API Key'
-# from langchain.llms import OpenAI
-from langchain_openai import OpenAI
-# llm = OpenAI(model_name="text-davinci-003",max_tokens=200)
-llm = OpenAI(model_name="gpt-3.5-turbo-instruct",max_tokens=200)
-# text = llm("请给我写一句情人节红玫瑰的中文宣传语")
+llm = ChatOllama(
+    base_url="http://localhost:11434",  # 服务地址
+    model="qwen3:8b"  # 模型名称
+)
+# 同步调用模型生成文本
 text = llm.invoke("请给我写一句情人节红玫瑰的中文宣传语")
 print(text)
